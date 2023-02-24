4e31592f0d03044f1bf1f7dfc375640d2c0056a9
sebbanwalker <seb10walker@hotmail.com>
2023-02-24 08:22:04 +0100

    friday 8:21

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let leftButton = document.getElementById('left-button');
 let rightButton = document.getElementById('right-button');
 let currentPlanet;
 
+//Loading all the API data.
 async function getSolarSystem() {
 
     resp = await fetch('https://majazocom.github.io/Data/solaris.json');
@@ -27,6 +28,8 @@ async function getSolarSystem() {
 
 getSolarSystem();
 
+
+//Opens the modal window and loads all planetary info from the API data. 
 function openPlanet(item) {
     console.log("I am clickabe: " + item.name);
     currentPlanet = item.id;
@@ -34,13 +37,35 @@ function openPlanet(item) {
     document.getElementById('modal-window').style.display = "flex";
     document.getElementById('planet-name').innerHTML = item.name;
     document.getElementById('desc--content').innerHTML = item.desc;
+    document.getElementById('image').style.backgroundImage = `url("assets/${item.name.toLowerCase()}.jpg")`;
+    document.getElementById('latin-name').innerHTML = item.latinName;
+    document.getElementById('moons').innerHTML = "";
+    if(item.moons.length > 0) {
+        for(i = 0; i < item.moons.length; i++) {
+            if (i < 4) {
+                if (i == item.moons.length-1) {
+                    document.getElementById('moons').innerHTML += item.moons[i];
+                } else {
+                    document.getElementById('moons').innerHTML += item.moons[i] + ", ";   
+                } 
+            } else if(i == 4) {
+                document.getElementById('moons').innerHTML += item.moons[i] + " m.fl...";
+            }
+        }
+    } else {
+        document.getElementById('moons').innerHTML += "Inga";
+    }
+
 }
 
+//Adds an eventlistener to the input field.
 if(searchInput) {
     console.log("Searchinput is true");
     searchInput.addEventListener("input", filterItems);
 }
 
+
+//Search filter that filters search results in a list in real-time.
 function filterItems() {
     const searchValue = searchInput.value.toLowerCase();
     const items = document.querySelectorAll('.search-item');
@@ -61,12 +86,11 @@ function filterItems() {
     }
 }
 
+//Opens the modal window for the chosen planet in the search list
 searchContainer.addEventListener('click', e => {
     if (e.target && e.target.classList.contains('search-item')) {
       let itemName = e.target.textContent;
-      //window.location.href = `https://example.com/search?query=${itemName}`;
       console.log(itemName);
-
       for(i = 0; i < data.length; i++) {
         if(e.target.textContent.toLowerCase() == data[i].name.toLowerCase()) {
             console.log("Found a matching planet:" + data[i].name.toLowerCase());
@@ -77,14 +101,17 @@ searchContainer.addEventListener('click', e => {
     }
   });
 
+//Event listener to leave the modal window
 backButton.addEventListener('click', function() {document.getElementById('modal-window').style.display = "none";})
 
+//Event listener to paginate left in the solar system
 leftButton.addEventListener('click', function() {
     if(currentPlanet > 0) {
         openPlanet(data[currentPlanet-1]);
     }
 })
 
+//Event listener to paginate right in the solar system
 rightButton.addEventListener('click', function() {
     if(currentPlanet < 8) {
         openPlanet(data[currentPlanet+1]);

3734037de287570336ea26d6393ea18c63b25080
sebbanwalker <seb10walker@hotmail.com>
2023-02-24 06:57:07 +0100

    friday morning

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let resp;
 let data;
 let searchContainer = document.getElementById('search-results');
 let searchInput = document.getElementById('search-input');
+let backButton = document.getElementById('back-button');
+let leftButton = document.getElementById('left-button');
+let rightButton = document.getElementById('right-button');
+let currentPlanet;
 
 async function getSolarSystem() {
 
@@ -11,13 +15,26 @@ async function getSolarSystem() {
 
 
     for(i = 0; i < data.length; i++) {
-        let item = data[i].name;
-        console.log(item);
+        let planet = document.getElementById(`${data[i].name.toLowerCase()}--planet`);
+        let item = data[i];
+        console.log(item.name);
+        planet.addEventListener('click', function() {
+            openPlanet(item);
+        });
     };
     
 }
 
 getSolarSystem();
+
+function openPlanet(item) {
+    console.log("I am clickabe: " + item.name);
+    currentPlanet = item.id;
+    console.log("Current planet ID: " + currentPlanet);
+    document.getElementById('modal-window').style.display = "flex";
+    document.getElementById('planet-name').innerHTML = item.name;
+    document.getElementById('desc--content').innerHTML = item.desc;
+}
 
 if(searchInput) {
     console.log("Searchinput is true");
@@ -49,5 +66,27 @@ searchContainer.addEventListener('click', e => {
       let itemName = e.target.textContent;
       //window.location.href = `https://example.com/search?query=${itemName}`;
       console.log(itemName);
+
+      for(i = 0; i < data.length; i++) {
+        if(e.target.textContent.toLowerCase() == data[i].name.toLowerCase()) {
+            console.log("Found a matching planet:" + data[i].name.toLowerCase());
+            document.getElementById('modal-window').style.display = "flex";
+            openPlanet(data[i]);
+        }
+      }
     }
   });
+
+backButton.addEventListener('click', function() {document.getElementById('modal-window').style.display = "none";})
+
+leftButton.addEventListener('click', function() {
+    if(currentPlanet > 0) {
+        openPlanet(data[currentPlanet-1]);
+    }
+})
+
+rightButton.addEventListener('click', function() {
+    if(currentPlanet < 8) {
+        openPlanet(data[currentPlanet+1]);
+    }
+})

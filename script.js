434f4667a387d61bf3ee42d87d862fd9df608550
sebbanwalker <seb10walker@hotmail.com>
2023-02-24 10:02:36 +0100

    friday 10:02

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ async function getSolarSystem() {
 
 
     for(i = 0; i < data.length; i++) {
+        console.log(data[i].name.toLowerCase());
         let planet = document.getElementById(`${data[i].name.toLowerCase()}--planet`);
         let item = data[i];
         console.log(item.name);
@@ -31,7 +32,7 @@ getSolarSystem();
 
 //Opens the modal window and loads all planetary info from the API data. 
 function openPlanet(item) {
-    console.log("I am clickabe: " + item.name);
+    console.log("I am clickable: " + item.name);
     currentPlanet = item.id;
     console.log("Current planet ID: " + currentPlanet);
     document.getElementById('modal-window').style.display = "flex";
@@ -40,7 +41,7 @@ function openPlanet(item) {
     document.getElementById('image').style.backgroundImage = `url("assets/${item.name.toLowerCase()}.jpg")`;
     document.getElementById('latin-name').innerHTML = item.latinName;
     document.getElementById('moons').innerHTML = "";
-    if(item.moons.length > 0) {
+    if(item.moons.length > 2) {
         for(i = 0; i < item.moons.length; i++) {
             if (i < 4) {
                 if (i == item.moons.length-1) {
@@ -48,14 +49,29 @@ function openPlanet(item) {
                 } else {
                     document.getElementById('moons').innerHTML += item.moons[i] + ", ";   
                 } 
-            } else if(i == 4) {
+            } else if (i == 4) {
                 document.getElementById('moons').innerHTML += item.moons[i] + " m.fl...";
+            } else {
+
             }
         }
-    } else {
+    }   else if(item.moons.length == 1) {
+        document.getElementById('moons').innerHTML += item.moons[0];
+    }   else if(item.moons.length == 2) {
+        document.getElementById('moons').innerHTML += item.moons[0] + ", ";
+        document.getElementById('moons').innerHTML += item.moons[1];
+    }   else {
         document.getElementById('moons').innerHTML += "Inga";
     }
 
+    document.getElementById('orbit').innerHTML = item.orbitalPeriod + " dagar";
+    document.getElementById('distance').innerHTML = item.distance + " km";
+
+    document.getElementById('temp').innerHTML = "Dag: " + item.temp.day + ", Natt: " + item.temp.night;
+
+
+
+    console.log("I am at the end of openPlanet.");
 }
 
 //Adds an eventlistener to the input field.
@@ -67,6 +83,7 @@ if(searchInput) {
 
 //Search filter that filters search results in a list in real-time.
 function filterItems() {
+    console.log("filterItems is running");
     const searchValue = searchInput.value.toLowerCase();
     const items = document.querySelectorAll('.search-item');
 
@@ -84,12 +101,13 @@ function filterItems() {
             }
         });
     }
+    console.log("filterItems is done.");
 }
 
 //Opens the modal window for the chosen planet in the search list
 searchContainer.addEventListener('click', e => {
     if (e.target && e.target.classList.contains('search-item')) {
-      let itemName = e.target.textContent;
+      let itemName = e.target.textContent.toLowerCase();
       console.log(itemName);
       for(i = 0; i < data.length; i++) {
         if(e.target.textContent.toLowerCase() == data[i].name.toLowerCase()) {
